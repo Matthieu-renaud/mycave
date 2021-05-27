c3a604d62cbfcb80b8653d54349f9a9721f35f9c
Matthieu RENAUD <matthieurenaud07@gmail.com>
2021-05-27 15:11:56 +0200

    un message

--- index.php
+++ index.php
@@ -14,10 +14,10 @@
 <header>
     <nav>
       <ul class="group_menus">
-        <li class="menus"><a href="./index.php">Accueil</a></li>
+        <li class="menus"><a href="./index.php">MyCave</a></li>
       </ul>
       <ul class="group_menus">
-        <li class="menus"><a href="./aff_article.php">Articles</a></li>
+        <li class="menus"><a href="./aff_article.php">Vins</a></li>
         <li class="menus"><a href="./ajout_article.php">Ajout</a></li>
       </ul>
       <ul class="group_menus">
@@ -30,7 +30,7 @@
 
   <main>
 
-    <div class="info">Bonjour,<br>Bienvenue sur un site utilisant php pour de l'exploitation de base de données</div>
+    <div class="info">Bonjour,<br>Bienvenue sur MyCave, un site créé par un expert en œnologie afin de réferencer uniquement la crème de la crème du vin.</div>
 
   </main>
 
