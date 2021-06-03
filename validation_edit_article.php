db025747f12adbab67c33ef65c23336d249a0ef2
Matthieu RENAUD <matthieurenaud07@gmail.com>
2021-06-03 09:33:12 +0200

    ajout favicon et fix titre de page

--- validation_edit_article.php
+++ validation_edit_article.php
@@ -4,8 +4,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Validation Édition Tag</title>
+  <title>My Cave - Les Vins</title>
   <link rel="stylesheet" href="./style.css">
+  <link rel="shortcut icon" href="./assets/img/favicon.png" type="image/x-icon">
 </head>
 <body>
 
